c0463488864a48bb62f9ef7d1e17264cfb673a19
grizzly21 <makintoch2104@gmail.com>
2023-06-18 17:57:08 +0300

    improved template and added simple checker for password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PasswordInputComponent } from './password-input/password-input.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { PasswordInputComponent } from './password-input/password-input.componen
     PasswordInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/password-input/password-input.component.ts
+++ src/app/password-input/password-input.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {debounceTime, distinctUntilChanged, fromEvent, map, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-password-input',
   templateUrl: './password-input.component.html',
   styleUrls: ['./password-input.component.css']
 })
-export class PasswordInputComponent {
+export class PasswordInputComponent implements AfterViewInit, OnDestroy{
+  @ViewChild('passwordInput') passwordInput?: ElementRef;
 
+  password: string = '';
+  passwordStrength!: PasswordStrength;
+  private inputSub$!: Subscription;
+
+  ngAfterViewInit() {
+    const input$ = fromEvent<any>(
+      this.passwordInput?.nativeElement as HTMLInputElement,
+      'input')
+      .pipe(
+        map(event => event.target?.value),
+        debounceTime(400),
+        distinctUntilChanged()
+      );
+    this.inputSub$ = input$.subscribe(
+      res => {
+        this.password = res;
+        this.checkPasswordStrength()
+      }
+    )
+  }
+
+  checkPasswordStrength(){
+    if(!this.password){
+      this.passwordStrength = PasswordStrength.EMPTY;
+    }else if (this.password.length < 8){
+      this.passwordStrength = PasswordStrength.SHORT
+    }else if (
+      /[A-Za-z]/.test(this.password) &&
+      /[0-9]/.test(this.password) &&
+      /[!@#$%^&*]/.test(this.password)
+    ){
+      this.passwordStrength = PasswordStrength.STRONG
+    }else {
+      this.passwordStrength = PasswordStrength.MEDIUM
+    }
+
+    console.log("password strength is :", PasswordStrength[this.passwordStrength])
+  }
+
+  ngOnDestroy() {
+    this.inputSub$.unsubscribe();
+  }
+}
+
+enum PasswordStrength {
+  EMPTY,
+  SHORT,
+  EASY,
+  MEDIUM,
+  STRONG
 }
