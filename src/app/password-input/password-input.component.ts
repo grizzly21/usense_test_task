620b0a6a13c0913b164f8aa07e69183d53baa81b
grizzly21 <makintoch2104@gmail.com>
2023-06-18 17:57:27 +0300

    improved template and added simple checker for password

--- src/app/password-input/password-input.component.ts
+++ src/app/password-input/password-input.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 import {debounceTime, distinctUntilChanged, fromEvent, map, Subscription} from "rxjs";
 
 @Component({
